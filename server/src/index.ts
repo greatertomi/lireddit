ff8a1498096ad72ecbd84238e1eac2e61be96fdd
John Oshalusi <oshalusijohn@gmail.com>
2021-11-09 00:26:15 +0100

    Create migration

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,8 +3,8 @@ import {Post} from "./entities/Post";
 import microConfig from './mikro-orm.config';
 
 const main = async () => {
-  const orm = await MikroORM.init(microConfig)
-
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
   const post = orm.em.create(Post, {title: 'My first post'});
   await orm.em.persistAndFlush(post);
 }
