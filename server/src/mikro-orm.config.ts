59d276a66f337c84a9672912d71019b18a4f69d6
John Oshalusi <oshalusijohn@gmail.com>
2021-11-10 23:50:17 +0100

    Began working on authentication

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -2,16 +2,18 @@ import {__prod__} from "./constants";
 import {Post} from "./entities/Post";
 import {MikroORM} from "@mikro-orm/core";
 import path from "path";
+import {User} from "./entities/User";
 
+const dirName = "C:\\Users\\John\\WebstormProjects\\lireddit\\server\\src"
 export default {
   dbName: 'lireddit',
   user: 'postgres',
   password: '01programmer01',
   debug: !__prod__,
   type: 'postgresql',
-  entities: [Post],
+  entities: [Post, User],
   migrations: {
-    path: path.join(__dirname, './migrations'),
+    path: path.join(dirName, "./migrations"),
     pattern: /^[\w-]+\d+\.[tj]s$/
   }
 } as Parameters<typeof MikroORM.init>[0];
